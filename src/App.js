8d12c55850dff3b1e880d975b217fdc6d88706fd
Vale <vale@MacBook-Pro.local>
2020-05-03 21:17:19 +0200

    latest fixes api calls

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss';
 import axios from 'axios';
+
 const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_KEY;
 
-function App() {
-  const [tvSeries, setTvSeries] = useState('');
 
-  const movieURL = `https://api.themoviedb.org/3/trending/tv/day?api_key=${MOVIE_API_KEY}`
+function App() {
+  const [tvSeriesName, setTvSeriesName] = useState('');
+  const [imgCover, setImgCover] = useState('');
 
   useEffect (() => {
+    const movieURL = `https://api.themoviedb.org/3/trending/tv/day?api_key=${MOVIE_API_KEY}&page=1`
+
     axios
       .get(movieURL)
       .then((res) => {
-          setTvSeries(res.data.data);
-          console.log(res);
+          setTvSeriesName(res.data.results[0].name);
       }) 
       .catch(e => console.log('error while loading data', e));
+
+      let PROXY = `https://cors-anywhere.herokuapp.com/`;
+      let imgUrl = `https://image.tmdb.org/t/p/w500/`;
+
+      axios
+        .get(movieURL)
+        .then((res) => {
+          setImgCover(imgUrl + res.data.results[0].poster_path)
+        })
+        .catch(e => console.log('error while fetching image!', e))
   })
 
   return (
@@ -24,33 +36,31 @@ function App() {
         <div className="hero-body">
           <div className="container">
             <h1 className="title">TV App <span role="img" aria-label="movie-emoji">🎥</span></h1>
-            <h2 className="subtitle">Find cool TV Series.</h2>
-            {/* <div className="result">{tvSeries}</div> */}
+            <h2 className="subtitle">Find epic TV Series.👌</h2>
           </div>
         </div>
       </section>
 
       <div className="columns">
         <div className="column">
-          <p className="title">Latest</p>
+          <p className="title">{tvSeriesName}</p>
             <div className="card">
             <div className="card-image">
               <figure className="image">
-                <img src="https://image.tmdb.org/t/p/w220_and_h330_face/qcr9bBY6MVeLzriKCmJOv1562uY.jpg" alt="" />
+                <img src={imgCover} alt="img" />
               </figure>
             </div>
             <div className="card-content">
-              {/* <button type="button" className="education" onClick={(e) => this.getBoredData(e)}>Get Activity</button> */}
-              {/* <p className="result">{tvSeries}</p> */}
+              <p className="result"></p>
             </div>
           </div>
         </div>
         <div className="column">
-          <p className="title">Latest</p>
+          <p className="title">{tvSeriesName}</p>
             <div className="card">
             <div className="card-image">
               <figure className="image">
-                <img src="https://image.tmdb.org/t/p/w220_and_h330_face/qcr9bBY6MVeLzriKCmJOv1562uY.jpg" alt="" />
+                <img src={imgCover} alt="img" />
               </figure>
             </div>
             <div className="card-content">
@@ -58,11 +68,11 @@ function App() {
           </div>
         </div>
         <div className="column">
-          <p className="title">Latest</p>
+          <p className="title">{tvSeriesName}</p>
             <div className="card">
             <div className="card-image">
               <figure className="image">
-                <img src="https://image.tmdb.org/t/p/w220_and_h330_face/qcr9bBY6MVeLzriKCmJOv1562uY.jpg" alt="" />
+                <img src={imgCover} alt="img" />
               </figure>
             </div>
             <div className="card-content">
