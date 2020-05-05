b1988092d9c1e2a3d03b1562532c399a2a133f4c
Vale <vale@MacBook-Pro.local>
2020-05-05 11:19:19 +0200

    fixed show more less btn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss';
 import axios from 'axios';
+import ReactTextCollapse from 'react-text-collapse';
 
 const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_KEY;
+
+const TEXT_COLLAPSE_OPTIONS = {
+  collapse: false,
+  collapseText: '...show more',
+  expandText: 'show less',
+  minHeight: 70,
+  maxHeight: 180,
+  textStyle: {
+    backgroundColor: 'black',
+    margin: '10px',
+    color: 'white',
+    fontSize: '18px',
+    float: 'center'
+  }
+}
 
 function App() {
   const [TvData, setTvData] = useState([]);
@@ -21,22 +37,12 @@ function App() {
     }, [])
 
     const renderData = () => {
-      const desc = document.querySelectorAll('p.overview');
-      const observer = new ResizeObserver(entries => {
-        for (let entry of entries) {
-          entry.target.classList[entry.target.scrollHeight > entry.contentRect.height ? 'add' : 'remove']('truncated');
-        }
-      });
-
-      desc.forEach(p => {
-        observer.observe(p);
-      });
-
-      let imgUrl = `https://image.tmdb.org/t/p/w300_and_h450_bestv2/`;
+  
+    const imgUrl = `https://image.tmdb.org/t/p/w300_and_h450_bestv2/`;
 
       return TvData.map(info => {
         return (
-            <div class="column">
+            <div className="column" key={info.id}>
               <div className="card">
                 <p className="title">{info.name}</p>
                 <div className="card-content">
@@ -47,16 +53,18 @@ function App() {
                   </div>
                   <div className="card-text">
                     <div className="box">
-                      <input type="checkbox" id="expanded" />
-                      <p className="overview">{info.overview}</p>  
-                      <label for="expanded" role="button">read more</label>
+                      <ReactTextCollapse options={TEXT_COLLAPSE_OPTIONS} style={{color: "red"}}>
+                        <p>
+                          {info.overview}
+                        </p>
+                      </ReactTextCollapse>
+                      <span className="score">Score:</span>
+                      <p className="vote">{info.vote_average}</p>
                     </div>
-                    <p className="vote">{info.vote_average}</p>
                   </div>
                 </div>
               </div>
             </div>
-          
         );
       }
     )}
